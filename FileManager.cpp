a3140cd42f1eb25f088c10b0f9a0d9d232411eab
Nadeen Ahmed <163456896+gharablynadeen@users.noreply.github.com>
2025-08-10 00:30:48 +0300

    import function done (not final)

--- FileManager.cpp
+++ FileManager.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
 #include<vector>
 #include<string>
-
+#include<fstream>
+using namespace std;
 
 class FileManager
 {
-    void export_to_filestring(string filename,vector<vector<double>& data, vector<string>columns, vector<string> comments)
+    void export(string filename,vector<vector<double>>& data, vector<string>columns, vector<string> comments)
     {
-        ofstream myfile
+        ofstream myfile ;
         myfile.open (filename);
 
       
-        for( int i=0;i<comments.size(),i++)
+        for( int i=0;i<comments.size();i++) 
         {
             if(comments[i]=="#")
                 myfile<<endl<<"#";
@@ -20,31 +21,80 @@ class FileManager
         }
             
         
-        for( int i=0;i<columns.size(),i++)
+        for( int i=0;i<columns.size();i++)
         {
             myfile<<columns[i]<<",";
         }
         
-        for( int i=0;i<data.size(),i++)
+        for( int i=0;i<data.size();i++)
         {
-            for(int j=0;i<data[0].size(),j++)
+            for(int j=0;i<data[0].size();j++)
             {
                  myfile <<data[i][j]<<",";
-                 
-            6
             }
             myfile<<endl<<endl;
         }
 
-
-  myfile.close()
+  myfile.close();
     }
-    vector<vector<double> import_from_file(string filename,istream& str)
-    {
-              vector<string>   result;
-                string line;
-                getline(str,line);
 
+    vector<vector<double>> import(string filename,istream& str)
+    {
+              /*vector<string>   result;
+                getline(str,line);*/
+
+                vector<string> titles ;
+                vector<vector<double>> data;
+                ifstream input ("filename");
+                string line;
+                double x;
+                int j = 0 ; //columns
+                int i = 0; //rows
+                int noOfColumns = 1;
+
+                input>> line ;
+
+                while (line == "#"){
+                     getline (input, line);
+                     cout<<"#"<<line;
+                     input>>line;
+                }
+                 
+                titles[0] = line ;
+                 
+                //getting values of each column title 
+                 while(input>>titles[1]){
+                noOfColumns += 1;
+                }
+
+                //cout values of each column title 
+                for(x=0;x<noOfColumns;x++){
+                    cout<<titles[x];
+                    if(x<noOfColumns-1)
+                    {
+                        cout<<",";
+                    }
+                    else 
+                    {
+                     cout<<endl<<endl;
+                    }
+                }
+
+                while(!input.eof())
+                {
+                  input >> data[i][j] ;
+                  if(j==noOfColumns-1){
+                  cout<<data[i][j]<<endl<<endl;
+                  i++;
+                  j=0;
+                  }
+                  else{
+                  cout<<data[i][j]<<",";
+                  j++;
+                }
+            }
+
+             input.close();   
 
     }
 };
