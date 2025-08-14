09697d36563d66bd7932d672694f156898a2571d
Nadeen Ahmed <163456896+gharablynadeen@users.noreply.github.com>
2025-08-14 16:12:34 +0300

    extracted text from comments

--- FileManager.cpp
+++ FileManager.cpp
@@ -129,4 +129,89 @@ bool FileManager::import_func(std::string filename, vector<vector<double>> &data
     input.close();
     return true;
 }
+
+bool FileManager::comment_extraction(vector<std::string> &comments,vector<unordered_map<string, std::variant>> &extracted )
+{
+    if(comments.empty())
+    {
+        cout << "No comments found." << endl;
+        return false;
+    }
+
+
+    for (int i = 0; i < comments.size(); i++)
+
     
+    {
+        int position = comments[i].find('=');
+        if (position == std::string::npos) // if '=' is not found in the comment
+            continue; // skip this comment
+
+            int leftpointer = position - 1;
+            int rightpointer = position + 1;
+            bool second_space_left= false;
+            bool second_space_right=false;
+
+            bool first_char_left= false;
+            bool first_char_right=false;
+            string key = "";
+            string value = "";
+
+            while (leftpointer >= 0 && !second_space_left)
+            {
+                comments[i][leftpointer] != ' ' ? first_char_left = true : first_char_left = false;
+
+                if (comments[i][leftpointer] != ' ')
+                {
+                    key = comments[i][leftpointer] + key;
+                    first_char_left = true;
+                     leftpointer--;
+                }
+
+                else if (first_char_left && comments[i][leftpointer] == ' ')
+                {
+                    second_space_left = true;
+                }   
+                
+                else
+                {
+                    leftpointer--;
+                }
+               
+            }
+
+             while (rightpointer < comments[i].size() && !second_space_right)
+            {
+                comments[i][rightpointer] != ' ' ? first_char_right = true : first_char_right = false;
+
+                if (comments[i][leftpointer] != ' ')
+                {
+                    value = comments[i][rightpointer] + value;
+                    first_char_right = true;
+                     rightpointer++;
+                }
+
+                else if (first_char_right && comments[i][rightpointer] == ' ')
+                {
+                    second_space_right = true;
+                }   
+                
+                else
+                {
+                    rightpointer++;
+                }
+               
+            }
+
+
+            
+        }
+    }
+
+
+
+    
+        
+    
+    return true;
+}
